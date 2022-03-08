53024bbc1bd1b264be955f4949d04198c512d131
Igor Popov <igor@igorpopov.io>
2022-03-08 17:05:07 +0200

    Add tests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ Tests/Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void CanEmbedParametersInQuery()
+        {
+            Assert.Fail();
+        }
+    }
+}
