634a4b2bcb72b772cd08ed4c3dc96bd3b2cdb957
Igor Popov <igor@igorpopov.io>
2022-03-08 16:59:54 +0200

    Add project files.

---
+++ NHibernateParameterizedQueryLogViewer/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace NHibernateParameterizedQueryLogViewer
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ NHibernateParameterizedQueryLogViewer/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace NHibernateParameterizedQueryLogViewer
+{
+    public class MainViewModel : ViewModel
+    {
+        public string Input { get; set; } = string.Empty;
+        public string Output { get; set; } = string.Empty;
+
+        public MainViewModel()
+        {
+            PropertyChanged += EmbedQueryParameters;
+        }
+
+        private void EmbedQueryParameters(object? sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName != nameof(Input)) return;
+
+            Output = Input;
+        }
+    }
+}
